e5848d73371f3620d2179a3af76ba81ed27d2788
it174963 <73958003+it174963@users.noreply.github.com>
2021-12-04 18:55:03 +0200

    Update methods.php

--- methods.php
+++ methods.php
@@ -21,7 +21,7 @@ if ($con->connect_errno) {
 
 //------------------------------------------------------------------------------
 $method = $_SERVER['REQUEST_METHOD'];
-$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+$request = 'cards';
 $input = json_decode(file_get_contents('php://input'),true);
 
 switch ($r=array_shift($request)) {
