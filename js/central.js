956a7bdd7265107c9808129f3909a106f45bc89b
it174963 <73958003+it174963@users.noreply.github.com>
2021-12-04 12:29:24 +0200

    Update central.js

--- js/central.js
+++ js/central.js
@@ -74,7 +74,7 @@ function shuffle(o) {
 
 function fill_board(){
   	$.ajax({url: "methods.php",
-            method: 'PUT',
+            method: 'POST',
             data:  { "symbol": "Hearts" , "number":  "A" } ,
             success: fill_real_board });
 }
