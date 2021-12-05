4e020c273b8069b499a18f5c373956cdd8bf425e
it174963 <73958003+it174963@users.noreply.github.com>
2021-12-05 14:24:53 +0200

    Update central.js

--- js/central.js
+++ js/central.js
@@ -76,7 +76,7 @@ function fill_board(){
   var data =JSON.stringify( { symbol: "Hearts" , number:  "A" } );
 
   $.ajax({url: "methods.php",
-          method: 'PU',
+          method: 'PUT',
           dataType: "json",
           contentType: "application/json" ,
           data:  data ,
