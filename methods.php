4bf90fc45b685d3a9f5cd55c31540a3e60978865
it174963 <73958003+it174963@users.noreply.github.com>
2021-12-18 10:34:04 +0200

    Update methods.php

--- methods.php
+++ methods.php
@@ -7,8 +7,12 @@ require_once "include/db_connect.php";
 // $method = $_SERVER['REQUEST_METHOD'];
 // $request = 'cards';
 // $input = json_decode(file_get_contents('php://input'));
-$input = file_get_contents('php://input');
-print $input['symbol'];
+// $input = file_get_contents('php://input');
+// print $input['symbol'];
+
+$json = '["geeks", "for", "geeks"]';
+  $data = json_decode($json);
+  echo $data[0];
 
 
 // switch ($request) {
