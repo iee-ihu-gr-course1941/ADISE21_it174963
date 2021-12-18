e93bcd74304948e7762650634531229dad1ec30a
it174963 <73958003+it174963@users.noreply.github.com>
2021-12-18 10:31:08 +0200

    Update methods.php

--- methods.php
+++ methods.php
@@ -7,7 +7,7 @@ require_once "include/db_connect.php";
 // $method = $_SERVER['REQUEST_METHOD'];
 // $request = 'cards';
 // $input = json_decode(file_get_contents('php://input'));
-$input = json_decode(file_get_contents('php://input'));
+$input = file_get_contents('php://input');
 print $input['symbol']];
 
 
