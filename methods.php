7f67fcaffa3e6eb49b308eb480f34fb66df51814
it174963 <73958003+it174963@users.noreply.github.com>
2021-12-18 21:35:37 +0200

    Update methods.php

--- methods.php
+++ methods.php
@@ -21,18 +21,18 @@ switch ($r=array_shift($request)) {
 }
 
 function handle_cards_clear(){
-  $sql = "UPDATE 'board_1' B1
-  		    INNER JOIN 'board_empty' BE ON 'B1.x'='BE.x' AND 'B1.y'='BE.y'
-          SET 'B1.c_symbol' = 'BE.c_symbol' , 'B1.c_number' = 'BE.c_number' ;" ;
+  $sql = "UPDATE 'board_1'
+  		    INNER JOIN 'board_empty'  ON 'board_1.x'='board_empty.x' AND 'board_1.y'='board_empty.y'
+          SET 'board_1.c_symbol' = 'board_empty.c_symbol' , 'board_1.c_number' = 'board_empty.c_number' ;" ;
     if (mysqli_query($conn, $sql)) {
       echo "Record updated successfully ";
     } else {
       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
 
-    $sql = "UPDATE 'board_2' B2
-            INNER JOIN 'board_empty' BE ON 'B2.x'='BE.x' AND 'B2.y'='BE.y'
-            SET 'B2.c_symbol' = 'BE.c_symbol' , 'B2.c_number' = 'BE.c_number' ;" ;
+    $sql = "UPDATE 'board_2'
+            INNER JOIN 'board_empty'  ON 'board_2.x'='board_empty.x' AND 'board_2.y'='board_empty.y'
+            SET 'board_2.c_symbol' = 'board_empty.c_symbol' , 'board_2.c_number' = 'board_empty.c_number' ;" ;
       if (mysqli_query($conn, $sql)) {
         echo "Record updated successfully ";
       } else {
