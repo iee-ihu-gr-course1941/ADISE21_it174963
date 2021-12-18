befeb9eb4044b730eb5870fb8f6131a07ca9a82e
it174963 <73958003+it174963@users.noreply.github.com>
2021-12-18 22:14:05 +0200

    Update methods.php

--- methods.php
+++ methods.php
@@ -22,8 +22,8 @@ switch ($r=array_shift($request)) {
 
 function handle_cards_clear(){
   $sql = "UPDATE `board_1`
-          INNER JOIN `board_empty`  ON board_1.`x`=board_empty.`x` AND board_1.`y`=board_empty.`y`
-          SET board_1.`c_symbol` = board_empty.`c_symbol` , board_1.`c_number` = board_empty.`c_number` ;" ;
+          INNER JOIN `board_empty`  ON `board_1`.`x`=`board_empty`.`x` AND `board_1`.`y`=`board_empty`.`y`
+          SET `board_1`.`c_symbol` = `board_empty`.`c_symbol` , `board_1`.`c_number` = `board_empty`.`c_number` ;" ;
     if (mysqli_query($conn, $sql)) {
       echo "Record updated successfully ";
     } else {
