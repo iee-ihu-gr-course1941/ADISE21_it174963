0003f0b659a098394bce7d148990c488a004da0f
it174963 <73958003+it174963@users.noreply.github.com>
2021-12-18 23:27:03 +0200

    Update methods.php

--- methods.php
+++ methods.php
@@ -15,8 +15,7 @@ switch ($r=array_shift($request)) {
 	case 'cards_2': handle_cards_2($method, $request, $data, $conn);
 									break;
   case 'cards_clear': handle_cards_clear($method, $request, $conn);
-                      echo "going to";
-                  break;
+                      break;
   default:  header("HTTP/1.1 404 Not Found");
             exit;
 }
@@ -27,7 +26,7 @@ function handle_cards_clear(){
   //         SET `c_symbol` = ''  ,  `c_number` = ''
   //         WHERE `x` = 1 AND `y`=1 " ;
 
-  $sql = "INSERT INTO `board_1`(`x`, `y`, `c_symbol`, `c_number`) VALUES (3,3,'Hearts','4');";
+  $sql = "INSERT INTO `board_1`(`x`, `y`, `c_symbol`, `c_number`) VALUES (3,3,'Hearts','4')";
 		if (mysqli_query($conn, $sql)) {
 			echo "Record updated successfully ";
 		} else {
