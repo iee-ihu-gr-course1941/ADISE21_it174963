b7b883fbe97a6e6442155591a89bf65f48555376
it174963 <73958003+it174963@users.noreply.github.com>
2021-12-18 10:43:36 +0200

    Update methods.php

--- methods.php
+++ methods.php
@@ -4,30 +4,30 @@ require_once "include/db_connect.php";
 
 
 //------------------------------------------------------------------------------
-// $method = $_SERVER['REQUEST_METHOD'];
-// $request = 'cards';
+$method = $_SERVER['REQUEST_METHOD'];
+$request = 'cards';
 $json = file_get_contents('php://input');
 $data = json_decode($json);
-echo $data->symbol;
+// echo $data->symbol;
 
-// switch ($request) {
-//   case 'cards': handle_cards($method, $request,$input);
-//             		break;
-//   default:  header("HTTP/1.1 404 Not Found");
-//             exit;
-// }
+switch ($request) {
+  case 'cards': handle_cards($method, $request,$data);
+            		break;
+  default:  header("HTTP/1.1 404 Not Found");
+            exit;
+}
 
-// function handle_cards($method, $request,$input){
-	// $sym=$input['symbol'];
-	// $num=$input['number'];
-	//
-	// if(!isset($sym)) {
-	// 	if(!isset($num)) {
-	// 		header("HTTP/1.1 400 Bad Request");
-	// 		print json_encode(["No data given."]);
-	// 		exit;
-	// 	}
-	// }
+function handle_cards($method, $request,$data){
+	$sym=$data['symbol'];
+	$num=$data['number'];
+
+	if(!isset($sym)) {
+		if(!isset($num)) {
+			header("HTTP/1.1 400 Bad Request");
+			print json_encode(["No data given."]);
+			exit;
+		}
+	}
 	//
 	// $sql = 'INSERT INTO board_1(x, y, c_symbol, c_number) VALUES (1,1,?,?) ';
 	// $st = $mysqli->prepare($sql);
