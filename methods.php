be622a8fcab8271ad9a03d4792ab0d74a2634252
it174963 <73958003+it174963@users.noreply.github.com>
2021-12-18 23:47:55 +0200

    Update methods.php

--- methods.php
+++ methods.php
@@ -21,28 +21,25 @@ switch ($r=array_shift($request)) {
 }
 
 function handle_cards_clear($method, $request, $conn){
-  // $sql = "UPDATE 'board_1'
-  //         INNER JOIN board_empty  ON `board_1.x` = `board_empty.x` AND `board_1.y`=`board_empty.y`
-  //         SET `c_symbol` = ''  ,  `c_number` = ''
-  //         WHERE `x` = 1 AND `y`=1 " ;
 
-  $sql =  "UPDATE board_2
-INNER JOIN board_empty  ON board_2.x=board_empty.x AND board_2.y=board_empty.y
-SET board_2.c_symbol = board_empty.c_symbol , board_2.c_number = board_empty.c_number";
-		if (mysqli_query($conn, $sql)) {
-			echo "Record updated successfully ";
-		} else {
-			echo "Error: " . $sql . "<br>" .  mysqli_error($conn);
-		}
 
-    // $sql = "UPDATE 'board_2'
-    //         INNER JOIN 'board_empty'  ON 'board_2.x'='board_empty.x' AND 'board_2.y'='board_empty.y'
-    //         SET 'board_2.c_symbol' = 'board_empty.c_symbol' , 'board_2.c_number' = 'board_empty.c_number' ;" ;
-    //   if (mysqli_query($conn, $sql)) {
-    //     echo "Record updated successfully ";
-    //   } else {
-    //     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    //   }
+  $sql = "UPDATE board_1
+          INNER JOIN board_empty  ON 'board_1.x' = 'board_empty.x' AND 'board_1.y' = 'board_empty.y'
+          SET 'board_1.c_symbol' = 'board_empty.c_symbol' , 'board_1.c_number' = 'board_empty.c_number' ";
+	if (mysqli_query($conn, $sql)) {
+		echo "Record updated successfully ";
+	} else {
+		echo "Error: " . $sql . "<br>" .  mysqli_error($conn);
+	}
+
+  $sql = "UPDATE board_2
+          INNER JOIN board_empty  ON 'board_2.x' = 'board_empty.x' AND 'board_2.y' = 'board_empty.y'
+          SET 'board_2.c_symbol' = 'board_empty.c_symbol' , 'board_2.c_number' = 'board_empty.c_number' ";
+	if (mysqli_query($conn, $sql)) {
+		echo "Record updated successfully ";
+	} else {
+		echo "Error: " . $sql . "<br>" .  mysqli_error($conn);
+	}
 
 
 }
