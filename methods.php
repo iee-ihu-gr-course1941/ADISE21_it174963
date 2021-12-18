4abf0b307d9579b1e1caf5636581badd1999028c
it174963 <73958003+it174963@users.noreply.github.com>
2021-12-18 11:37:18 +0200

    Update methods.php

--- methods.php
+++ methods.php
@@ -37,7 +37,7 @@ function handle_cards($method, $request, $data, $conn){
 
 //-----------ΦΘΑΝΟΥΝ ΜΕΧΡΙ ΕΔΩ ΟΙ ΤΙΜΕΣ ΠΟΥ ΘΕΛΩ--------------------------------
 
-	$sql = "INSERT INTO `board_1`(`x`, `y`, `c_symbol`, `c_number`) VALUES (1,1,'$sym','$num') ;" ;
+	$sql = "UPDATE `board_1` SET `c_symbol`='Hearts',`c_number`='4' WHERE `x`=1 AND `y`=1 ;" ;
 	if (mysqli_query($conn, $sql)) {
 		echo "New record created successfully";
 	} else {
