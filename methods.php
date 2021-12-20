0773e0753d3e32d8fa0a2aed5e0ec396abefa77b
it174963 <73958003+it174963@users.noreply.github.com>
2021-12-21 01:19:30 +0200

    Update methods.php

--- methods.php
+++ methods.php
@@ -7,7 +7,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 $request = explode ('/',trim($_SERVER['PATH_INFO'],'/'));
 $json = file_get_contents('php://input');
 $data = json_decode($json);
-echo "$data";
+echo "$data['token']";
 
 if(isset($_SERVER['HTTP_X_TOKEN'])) {
     $data['token']=$_SERVER['HTTP_X_TOKEN'];
