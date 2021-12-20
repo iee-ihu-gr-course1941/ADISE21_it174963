082a4bdb6c202948d22a41a815efa4f03e7a6e83
it174963 <73958003+it174963@users.noreply.github.com>
2021-12-21 00:30:40 +0200

    Update methods.php

--- methods.php
+++ methods.php
@@ -36,7 +36,9 @@ function log_user($method, $request, $data, $conn){
 
   $sql1 = "SELECT `username`  FROM `players` WHERE `player_side`='$p_side'";
   $result = mysqli_query($conn, $sql1);
-  echo $result;
+  $row = mysql_fetch_row($result);
+
+  echo $row[0]; // 42
 
   // if (mysqli_query($conn, $sql2)) {
   //   $result = mysqli_query($conn, $sql1);
