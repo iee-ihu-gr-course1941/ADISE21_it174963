c8b6dc19db31a9503f63449f7f4cc2091d4de92c
it174963 <73958003+it174963@users.noreply.github.com>
2021-12-28 14:52:18 +0200

    Update methods.php

--- methods.php
+++ methods.php
@@ -30,7 +30,7 @@ switch ($r=array_shift($request)) {
 
 //---------STATUS SECTION-------------------------------------------------------
 function handle_status($conn) {
-	check_abort();
+	check_abort($conn);
 
 	$sql = "SELECT * FROM `game_status`" ;
   $result = mysqli_query($conn, $sql);
