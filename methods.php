a5760e98b6f08918e3f0b83ffb16db8f204999fb
it174963 <73958003+it174963@users.noreply.github.com>
2021-12-28 14:15:46 +0200

    Update methods.php

--- methods.php
+++ methods.php
@@ -34,13 +34,13 @@ function handle_status($conn) {
 
 	$sql = "SELECT * FROM `game_status`" ;
   $result = mysqli_query($conn, $sql);
-  if (mysqli_num_rows($result) > 0) {
+  // if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
-      echo "<br>" . "Status: " . $row;
+      print json_encode($row["status"]);
     }
-  } else {
-    echo "0 results";
-  }
+  // } else {
+  //   echo "0 results";
+  // }
 }
 
 
