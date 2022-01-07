389633f82c7791cb242f47975cce6b6306d74164
it174963 <73958003+it174963@users.noreply.github.com>
2022-01-07 23:08:03 +0200

    Update central.js

--- js/central.js
+++ js/central.js
@@ -346,10 +346,11 @@ function create_Cards(index , side , cell_1 , cell_2 , c_s , c_n){
 function fill_board_game(i, x, y, card_found) {
 
   var x = $(card_found).attr("id");
-  var cardToPass = x.substring(0, 11);
+  var cardToPass = x.find("span");
+  var cTP = x.substring(0, 11);
 
-  var cn = var_card[0].innerHTML;
-  var cs = var_card[1].innerHTML;
+  var cn = cardToPass[0].innerHTML;
+  var cs = cardToPass[1].innerHTML;
 
   switch (cs) {
     case "♣":
@@ -373,13 +374,13 @@ function fill_board_game(i, x, y, card_found) {
     number: cn
   });
 
-  fill_board_db(dataToPass , cardToPass);
+  fill_board_db(dataToPass , cTP);
 
 }
 //------------------------------------------------------------------------------
 
-function fill_board_db(dataToPass , cardToPass){
-  if(cardToPass == '#div_card_1_'){
+function fill_board_db(dataToPass , cTP){
+  if(cTP == '#div_card_1_'){
     var url_value = "methods.php/cards_1/"
   }else{
     var url_value = "methods.php/cards_2/"
